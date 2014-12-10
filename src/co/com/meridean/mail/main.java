1fdeae781dee478158e0c1e233c561e0a97069cb
jaanvagu <jaanvagu@hotmail.com>
2014-12-10 09:32:40 -0500

    commit inicial

--- src/co/com/meridean/mail/main.java
+++ src/co/com/meridean/mail/main.java
@@ -1,12 +1,13 @@
 package co.com.meridean.mail;
 
 
+import co.com.meridean.utils.Indicators;
 import co.com.meridean.utils.Util;
 
 public class main {
 
     public static void main(String[] args) throws Exception {
-        Util u = new Util();
-        u.deleteUrlsDuplicated();
+        Indicators indicators = new Indicators();
+        indicators.processLinesFileGold();
     }
 }
